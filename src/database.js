41edd13b3f7b10fe51d591e5dcd768fa157c3b2d
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-07-05 21:06:59 -0300

    Created the database json

--- src/database.js
+++ src/database.js
@@ -1,24 +1,47 @@
+import fs from 'node:fs/promises' //para trabalhar com arquivo físicos no Node
+
+//Uso uma classe interna do Node URL 
+//dois parametros -> o nome do arquivo e o caminho relativo
+const databasePath = new URL('db.json', import.meta.url);
+
 export class Database {
-  #database = {} //uso um objeto para adicionar qualquer coisa além de usuário
+  #database = {}
+
+  //Para recuperar os dados
+  constructor() {
+    //then é uma promise pois quando ele terminar de ler o arquivo por completo vou pegar os dados do arquivo e vou salvar dentro do banco de dados
+    fs.readFile(databasePath, 'utf-8')
+    .then(data => {
+      this.#database = JSON.parse(data)
+    }).catch(() => {
+      //Para fazer a tratativa uso catch e crio um arquivo mesmo que vazio
+      this.#persistent();
+    })
+  }
+
+  //Método que vai escrever o nosso banco de dados em um arquivo físico
+  #persistent(){
+    //Vou salvar em json pois é melhor para ler e escrever os dados
+    //JSON.stringify(this.#database) para converter o nosso banco em uma estrutura JSON
+    fs.writeFile(databasePath, JSON.stringify(this.#database)); //writeFile - Só aceita string
+  }
 
   select(table) {
-    //existe uma tabela com o nome que passei? se não retorno um Array
     const data = this.#database[table] ?? []
 
     return data;
   }
 
   insert(table, data) {
-    //Verificar se existe um registro inserido naquela tabela, se não existir vamos adicionar
-    //Verifico se já existe um array nessa tabela
+
     if (Array.isArray(this.#database[table])) {
-      //Vou pegar essa tabela e adicionar o novo item
       this.#database[table].push(data);
     } else {
-      //Se não eu vou criar um Array com o item ali dentro, pois não tenho nada inserido na tabela
       this.#database[table] = [data];
     }
-    //retorno o item que foi inserido
+    //Vai ser chamado toda vez que eu inserir uam informação no Banco de dados
+    this.#persistent();
+
     return data;
   }
 }
--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import http from 'node:http'
 import { json } from './middlewares/json.js'
 import { Database } from './database.js'
 
-//no lugar de user, agora database
 const database = new Database
 
 const server = http.createServer(async(req, res) => {
@@ -11,7 +10,6 @@ const server = http.createServer(async(req, res) => {
 	await json(req, res)
 
 	if(method === 'GET' && url === '/users') {
-		//Na buscagem vou buscar a listagem de usuários na minha tabela
 		const users = database.select('users');
 
 		return res.end(JSON.stringify(users))
@@ -19,16 +17,12 @@ const server = http.createServer(async(req, res) => {
 
 	if(method === 'POST' && url === '/users') {
 		const { name, email } = req.body
-
-		//Agora variável user
 		const user = {
 			id: 1,
 			name,
 			email
 		};
-
-		//já aparece as opções para mim
-		//nome da tabela e nome da informação
+		
 		database.insert('users', user)
 
 		return res.writeHead(201).end()
