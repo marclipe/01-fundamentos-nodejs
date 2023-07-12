7e06eef054ff96cc72c65ce25a05f86a514f5004
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-07-12 01:29:53 -0300

    Routes with params

--- src/middlewares/utils/build-route-path.js
+++ src/middlewares/utils/build-route-path.js
@@ -1,7 +1,11 @@
 ///users/:id
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
+  //Vou pegar minha regex e substituir por uma string
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
-  //Para conferir se a regex deu certo
-  console.log(Array.from(path.matchAll(routeParametersRegex)))
+  //Preciso que a minha regex comece com pathWithParams 
+  const pathRegex = new RegExp(`^${pathWithParams}`);
+
+  return pathRegex
 }
--- src/server.js
+++ src/server.js
@@ -8,10 +8,17 @@ const server = http.createServer(async(req, res) => {
   await json(req, res);
 
   const route = routes.find((route) => {
-    return route.method === method && route.path === url;
+    //Ao invés de verificar se é igual eu uso o método test
+    //Toda regex tem um método chamado text: pathRegex.test que retorne true ou false
+    //Aí passo a nossa url
+    return route.method === method && route.path.test(url)
   });
 
   if(route) {
+    //Agora eu quero ver os dados que a regex encontrou na minha rota
+    const routeParameters = req.url.match(route.path)
+    console.log(routeParameters)
+
 		return route.handler(req, res)
 	} 
 
